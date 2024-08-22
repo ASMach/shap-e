98ae7cf00e7cbe9d646d9221e93ef617973f7ef6
Alonzo Machiraju <machthree@gmail.com>
2024-08-22 10:23:08 -0700

    Command line test script in case of notebook failure

---
+++ shap_e/examples/image-to-model.py
@@ -0,0 +1,45 @@
+import torch
+import platform
+
+from shap_e.diffusion.sample import sample_latents
+from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
+from shap_e.models.download import load_model, load_config
+from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_widget
+from shap_e.util.image_util import load_image
+from shap_e.util.device import check_device
+
+device = check_device()
+
+xm = load_model('transmitter', device=device)
+model = load_model('image300M', device=device)
+diffusion = diffusion_from_config(load_config('diffusion'))
+
+batch_size = 4
+guidance_scale = 3.0
+
+# To get the best result, you should remove the background and show only the object of interest to the model.
+image = load_image("example_data/corgi.png")
+
+latents = sample_latents(
+    batch_size=batch_size,
+    model=model,
+    diffusion=diffusion,
+    guidance_scale=guidance_scale,
+    model_kwargs=dict(images=[image] * batch_size),
+    progress=True,
+    clip_denoised=True,
+    use_fp16=True,
+    use_karras=True,
+    karras_steps=64,
+    sigma_min=1e-3,
+    sigma_max=160,
+    s_churn=0,
+)
+
+render_mode = 'nerf' # you can change this to 'stf' for mesh rendering
+size = 64 # this is the size of the renders; higher values take longer to render.
+
+cameras = create_pan_cameras(size, device)
+for i, latent in enumerate(latents):
+    images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
+    display(gif_widget(images))
